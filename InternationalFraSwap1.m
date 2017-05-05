b9a942b936795c0ed3727f1449bafe2dc4002ed1
keith2000 <keith2000@gmail.com>
2017-05-05 16:04:18 +0200

    fer

--- InternationalFraSwap1.m
+++ InternationalFraSwap1.m
@@ -1,13 +1,18 @@
 clear all %#ok<CLSCR>
 close all
 drawnow
-currencyStr = 'USD';
 currencyStr = 'ZAR';
-%currencyStr = 'EUR';
+currencyStr = 'EUR';
+
+%currencyStr = 'GBP';
+
+%currencyStr = 'USD';
 
 
+%currencyStr = 'CHF';
 
-fraMaturityVec =   (1:48)/12;
+
+fraMaturityVec =   (3:48)/12;
 swapMaturityVec = 1:30;
 
 tickerNameVec = BloombergFraSwapTickersForBlock( currencyStr,...
@@ -16,8 +21,8 @@ tickerNameVec = BloombergFraSwapTickersForBlock( currencyStr,...
 securityStrCell = {tickerNameVec.ticker};
 fieldStrCell = {'px_last'};
 
-startDateVal = datenum('2012-01-20');
-endDateVal = datenum('2017-01-20');
+startDateVal = datenum('2016-03-01');
+endDateVal = datenum('2017-02-20');
 
 dateVector = startDateVal:endDateVal;
 
@@ -32,18 +37,28 @@ for dateLoop = length(dateVector):-1:1
         [bloomHistVec, responseHistStr] = ProfessorKettledrumRequest( securityStrCell , fieldStrCell, dateVal, dateVal, verbosityLevel );
         
         
+        fraSwapVec = MergeBloomResponseWithFraSwapStruct( tickerNameVec, bloomHistVec, dateVal);
+        
+        logicalVec =  ~isnan([fraSwapVec.px_last]) & [fraSwapVec.isSwap];
+        fprintf('swap maturities: ')
+        fprintf('%d ', [fraSwapVec(logicalVec).maturityInYears] )
+        fprintf('\nFRA maturities: ')
+        logicalVec =  ~isnan([fraSwapVec.px_last]) & ~[fraSwapVec.isSwap];
+        fprintf('%d ', 12*[fraSwapVec(logicalVec).maturityInYears] )
+        breakline
         
         
-         disp(  qdatestr(dateVal) )
-         
-         
-         stop
-%         if isempty(bloomHistVec)
-%             disp('nothing')
-%             stop
-%         else
-%             disp('something')
-%         end
+        
+        
+        
+        
+        stop
+        %         if isempty(bloomHistVec)
+        %             disp('nothing')
+        %             stop
+        %         else
+        %             disp('something')
+        %         end
         
         
     end
--- MergeBloomResponseWithFraSwapStruct.m
+++ MergeBloomResponseWithFraSwapStruct.m
@@ -1,9 +1,30 @@
-tickerNameVec
+function fraSwapVec = MergeBloomResponseWithFraSwapStruct( tickerNameVec, bloomHistVec, dateVal)
 
 
-bloomHistVec
+%bloomHistVec
 
+%dateVal = 736715;
 
 fraSwapVec = [];
 
-for jj = 1:length()
+for jj = 1:length(tickerNameVec)
+   
+    temp = tickerNameVec(jj);
+
+    temp.dateVal = dateVal;
+    
+    temp.px_last = NaN;
+    
+    thisStruct = bloomHistVec(  strcmp( {bloomHistVec.security}, temp.ticker )  );
+    %stop
+    if ~isempty(thisStruct)
+        if thisStruct.fieldVec.date == dateVal
+            temp.px_last = thisStruct.fieldVec.px_last/100;
+        end
+    end
+        
+        
+   fraSwapVec = [fraSwapVec, temp];
+   % stop
+    
+end
